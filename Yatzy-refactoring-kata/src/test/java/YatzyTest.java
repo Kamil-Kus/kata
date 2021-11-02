f6ce016541e5389188b7990e517cd0ac7527d98d
Kamil Kus <KamilKusDev@protonmail.com>
2021-11-02 14:00:59 +0100

    Refactor UT yatzy_scores_50

--- Yatzy-refactoring-kata/src/test/java/YatzyTest.java
+++ Yatzy-refactoring-kata/src/test/java/YatzyTest.java
@@ -14,19 +14,17 @@ public class YatzyTest {
 
     @Test
     public void yatzy_scores_50() {
-        int expected = 50;
-        int actual = Yatzy.yatzy(new DiceHand(4, 4, 4, 4, 4));
-        assertEquals(expected, actual);
+        assertEquals(50, Yatzy.yatzy(new DiceHand(4, 4, 4, 4, 4)));
         assertEquals(50, Yatzy.yatzy(new DiceHand(6, 6, 6, 6, 6)));
         assertEquals(0, Yatzy.yatzy(new DiceHand(6, 6, 6, 6, 3)));
     }
 
     @Test
     public void test_1s() {
-        assertTrue(Yatzy.ones(1, 2, 3, 4, 5) == 1);
-        assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
-        assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
-        assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
+        assertEquals(1, Yatzy.ones(new DiceHand(1, 2, 3, 4, 5)));
+        assertEquals(2, Yatzy.ones(new DiceHand(1, 2, 1, 4, 5)));
+        assertEquals(0, Yatzy.ones(new DiceHand(6, 2, 2, 4, 5)));
+        assertEquals(4, Yatzy.ones(new DiceHand(1, 2, 1, 1, 1)));
     }
 
     @Test
